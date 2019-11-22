d28cb4b0364c65c5da49629d00c365e45a0911ef
Nicole Kauer <nmekauer@gmail.com>
2019-11-22 15:03:57 -0800

    functions for getting file indices based on metadataType
    Moves `get_file_indices()` to get-file-indices.R file. Adds two functions for getting specific forms of the indices, a named list or a vector. Updates `get_file_indices()` to be a helper function to the two exported functions. Adds testing and documentation for the function.

--- R/get-num-ids.R
+++ R/get-num-ids.R
@@ -1,59 +1,52 @@
 #' Get number of individuals
 #'
 #' Get the number of unique individuals for a study
-#' from the metadata files.
+#' from the metadata files. Checks for ids in the manifest,
+#' individual metadata, and biospecimen metadata.
 #'
 #' @param study_view The file view for a study, with the
 #'   columns 'file_data', where each element is a tibble
 #'   with the data, and metadataType, where each element
 #'   is the metadataType for the file.
-#' @return Number of unique individual ids.
+#' @return Number of unique individual ids or 0 if no metadataTypes
+#'   individual, biospecimen, manifest exist in `study_view`.
 num_individuals <- function(study_view) {
   files_of_interest <- c("manifest", "individual", "biospecimen")
   file_indices <- get_file_indices(study_view, files_of_interest)
 
-  individuals <- list()
-  for (index in file_indices) {
-    individuals <- c(individuals, study_view$file_data[[index]]$individualID)
+  if (!is.null(file_indices)) {
+    individuals <- list()
+    for (index in file_indices) {
+      individuals <- c(individuals, study_view$file_data[[index]]$individualID)
+    }
+    num_individuals <- length(unique(individuals))
+  } else {
+    num_individuals <- 0
   }
-  length(unique(individuals))
+  num_individuals
 }
 
 #' Get number of specimens
 #'
 #' Get the number of unique specimens for a study
-#' from the metadata files.
+#' from the metadata files. Checks for ids in the manifest,
+#' biospecimen metadata, and assay metadata.
 #'
 #' @inheritParams num_individuals
-#' @return Number of unique specimen ids.
+#' @return Number of unique specimen ids or 0 if no metadataTypes
+#'   manifest, biospecimen, assay exist in `study_view`.
 num_specimens <- function(study_view) {
   files_of_interest <- c("manifest", "biospecimen", "assay")
   file_indices <- get_file_indices(study_view, files_of_interest)
 
-  specimens <- list()
-  for (index in file_indices) {
-    specimens <- c(specimens, study_view$file_data[[index]]$specimenID)
-  }
-  length(unique(specimens))
-}
-
-#' Get file indices by metadataType
-#'
-#' Get the file indices for `study_view` based on
-#' metadataType.
-#'
-#' @inheritParams num_individuals
-#' @param meta_types List of metadataTypes.
-#' @return List of indices of `study_view` for which
-#'   the metadataType exists.
-get_file_indices <- function(study_view, meta_types) {
-  file_indices <- purrr::map(
-    meta_types,
-    function(x) {
-      which(study_view$metadataType == x)
+  if (!is.null(file_indices)) {
+    specimens <- list()
+    for (index in file_indices) {
+      specimens <- c(specimens, study_view$file_data[[index]]$specimenID)
     }
-  )
-  # flatten removes integer(0) values for indices
-  file_indices <- purrr::flatten(file_indices)
-  file_indices
+    num_specimens <- length(unique(specimens))
+  } else {
+    num_specimens <- 0
+  }
+  num_specimens
 }
