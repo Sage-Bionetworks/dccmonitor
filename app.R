ca33b137a7aaccbd29e4782347036153956b0578
Dan Lu <danlu@ip-10-5-31-201.ec2.internal>
2022-05-10 15:58:18 +0000

    1kD settings

--- app.R
+++ app.R
@@ -2,7 +2,7 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
-Sys.setenv(R_CONFIG_ACTIVE = "default", # Replace "default" with your config
+Sys.setenv(R_CONFIG_ACTIVE = "1kD", # Replace "default" with your config
            R_CONFIG_FILE = "inst/config.yml")
 pkgload::load_all()
 options("golem.app.prod" = TRUE)
